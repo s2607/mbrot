96435cd918d68c717289553637f9c06b709e45ea
phreaker2600 <swwiley@gmail.com>
2015-04-08 19:44:56 +0000

    yay it works

--- main/mbrot.go
+++ main/mbrot.go
@@ -4,9 +4,9 @@ import "github.com/s2607/mandelbrot"
 import "fmt"
 
 func main() {
-	var scale int = 70
-	for i := 0; i < scale/1; i++ {
-		for rel := 0; rel < scale/1; rel++ {
+	var scale int = 79
+	for i := -scale; i < scale; i++ {
+		for rel := -scale; rel < scale; rel++ {
 			if mandelbrot.Mbi(float64(rel), float64(i), float64(scale)) {
 				fmt.Print("#")
 			} else {
