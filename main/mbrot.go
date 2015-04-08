711dc07297a2b0ab9f4e1af011d1848521f9d841
phreaker2600 <swwiley@gmail.com>
2015-04-08 19:32:51 +0000

    not quite

--- main/mbrot.go
+++ main/mbrot.go
@@ -4,5 +4,16 @@ import "github.com/s2607/mandelbrot"
 import "fmt"
 
 func main() {
-	fmt.Printf("hello %s\n", mandelbrot.S())
+	var scale int = 70
+	for i := 0; i < scale/1; i++ {
+		for rel := 0; rel < scale/1; rel++ {
+			if mandelbrot.Mbi(float64(rel), float64(i), float64(scale)) {
+				fmt.Print("#")
+			} else {
+				fmt.Print(".")
+			}
+		}
+		fmt.Println("<")
+	}
+	fmt.Println("done")
 }
--- mandelbrot.go
+++ mandelbrot.go
@@ -1,17 +1,23 @@
 package mandelbrot
 
 import "math/cmplx"
-import "fmt"
 
-func S() string {
-	var z, c complex128 = 0, 1
-	for i := 0; i < 10; i++ {
+func test(c complex128, depth int) bool {
+	var z complex128 = 0
+	for i := 0; i < depth; i++ {
 		z = cmplx.Pow(z, 2) + c
+		if real(z)+imag(z) > 2 {
+			return false
+		}
+
 	}
-
-	return fmt.Sprintf("a %v", z)
-
+	return true
 }
-func Four() int {
-	return 4
+func Mbi(x float64, y float64, scale float64) bool {
+	x = x - (scale / 2)
+	y = y - (scale / 2)
+	x = x * (1 / scale)
+	y = y * (1 / scale)
+
+	return test(complex(x, y), 10)
 }
